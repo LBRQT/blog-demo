7cb5224f3b6d36f3713f43404c8907283abf7463
Boris Souquière <souquiereb1998@gmail.com>
2022-09-20 16:36:43 +0200

    login signin ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,10 +25,11 @@ class UserController extends AbstractController
      /**
      * @Route("/add", name="app_user_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository, User $user): Response
+    public function new(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+        
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -40,10 +43,10 @@ class UserController extends AbstractController
             // cette méthode fait le persist et le flush !
             $userRepository->add($user, true);
 
-            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('back/user/new.html.twig', [
+        return $this->renderForm('user/new.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
